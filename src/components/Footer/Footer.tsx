3aa320b19757fffb5e1081377fcd0d57003ca764
Najmus-Shakib797 <nshakib797@gmail.com>
2026-09-13 21:16:52 +0600

    4th-commit

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,11 +8,7 @@ export default function Footer() {
           <div>
             {/* Logo */}
             <div className="flex items-center mb-4">
-              <img
-                src="/public/logo-text.png"
-                alt="Logo"
-                className="h-10 mr-2"
-              />
+              <img src="/logo-text.png" alt="Logo" className="h-10 mr-2" />
               <span className="text-lg font-semibold">Dev Stack</span>
             </div>
 
